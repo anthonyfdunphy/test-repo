251720c55f9b3b98252d46a996c2053e50ce9742
anthonyfdunphy <anthonyfdunphy@gmail.com>
2023-02-20 21:28:15 +0000

    settings add white noise

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
